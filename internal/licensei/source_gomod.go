bb3c69f48a73254424a6ba3425c79539aea40bd3
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-02-14 21:54:19 +0100

    Improve go module dependency listing

--- internal/licensei/source_gomod.go
+++ internal/licensei/source_gomod.go
@@ -1,11 +1,7 @@
 package licensei
 
 import (
-	"bufio"
-	"bytes"
-	"os/exec"
-	"strings"
-
+	"github.com/goph/licensei/pkg/pkgmgr/gomod"
 	"github.com/pkg/errors"
 )
 
@@ -17,40 +13,32 @@ func NewGoModDependencySource() *gomodDependencySource {
 }
 
 func (s *gomodDependencySource) Dependencies() ([]Dependency, error) {
-	modlistCmd := exec.Command("go", "list", "-deps", "-f", "{{with .Module}}{{if not .Main}}{{.Path}} {{.Version}}{{end}}{{end}}", "./...")
-
-	var buf bytes.Buffer
-
-	modlistCmd.Stdout = &buf
-
-	err := modlistCmd.Run()
+	deps, err := gomod.ListDeps("")
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to list modules: "+buf.String())
+		return nil, errors.Wrap(err, "failed to list dependencies")
 	}
 
-	var dependencies []Dependency
-	depMap := map[string]bool{}
+	var dependencies []Dependency // nolint: prealloc
+	moduleMap := map[string]bool{}
 
-	scanner := bufio.NewScanner(&buf)
-	for scanner.Scan() {
-		rawMod := scanner.Text()
-		if rawMod == "" {
+	for _, dep := range deps {
+		// Skip the main module and non-module dependencies
+		if dep.Module == nil || dep.Module.Main {
 			continue
 		}
 
-		mod := strings.Split(rawMod, " ")
-
-		if _, ok := depMap[mod[0]]; ok {
+		// Skip already processed modules
+		if _, ok := moduleMap[dep.Module.Path]; ok {
 			continue
 		}
 
-		depMap[mod[0]] = true
+		moduleMap[dep.Module.Path] = true
 
 		dependencies = append(
 			dependencies,
 			Dependency{
-				Name:     mod[0],
-				Revision: mod[1],
+				Name:     dep.Module.Path,
+				Revision: dep.Module.Version,
 				Type:     "gomod",
 			},
 		)
---
+++ pkg/pkgmgr/gomod/testdata/mod/go.mod
@@ -0,0 +1,6 @@
+module github.com/goph/licensei/pkg/pkgmgr/gomod/testdata/mod
+
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+)
